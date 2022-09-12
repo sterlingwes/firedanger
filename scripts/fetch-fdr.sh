c64de7b6e0fc29b86c317ace18c77e4ee9f95b59
Wes Johnson <wes@swj.io>
2022-09-11 21:36:19 -0400

    add ci publish

--- scripts/fetch-fdr.sh
+++ scripts/fetch-fdr.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 SINCE_DATE=$(date '+%b %d %Y')
-
 curl -z "$SINCE_DATE" https://cwfis.cfs.nrcan.gc.ca/downloads/fire_danger/fdr.zip --output files/fdr.zip
 
-node scripts/convert-geojson.js > files/fdr.json
+if [ -f files/fdr.zip ]
+then
+  node scripts/convert-geojson.js > files/fdr.json
+
+  REF_DATE=$(date '+%F')
+  echo "{\"date\":\"$REF_DATE\"}" > files/date.json
+
+else
+  echo "No FDR file downloaded for $SINCE_DATE"
+  exit 1
+fi
+
+
+# yarn wrangler r2 object put firedanger/fdr-2022-09-11 -f files/fdr.json --content-type "application/json"
